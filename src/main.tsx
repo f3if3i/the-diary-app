298214431b64063f630c8fe9417df1509e7570b0
lottie0914-faye <lottie0914@outlook.com>
2022-08-16 09:36:45 +0900

    feat: setup browserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter } from "react-router-dom"
 import App from "./App"
 import "./index.css"
 import { ChakraProvider } from "@chakra-ui/react"
@@ -7,7 +8,9 @@ import { ChakraProvider } from "@chakra-ui/react"
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <ChakraProvider>
-            <App />
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
         </ChakraProvider>
     </React.StrictMode>
 )
