400a9d3179400e4d8aae56eeb062eacbf58d0b5b
lottie0914-faye <lottie0914@outlook.com>
2022-08-18 14:04:51 +0900

    fix: storybookがemotionを読み取れない問題を解決

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,3 +1,5 @@
+const react = require("@vitejs/plugin-react");
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -14,5 +16,24 @@ module.exports = {
   },
   "features": {
     "storyStoreV7": true
-  }
+  },
+  async viteFinal(config) {
+    config.plugins = config.plugins.filter(
+      (plugin) =>
+        !(Array.isArray(plugin) && plugin[0]?.name.includes("vite:react"))
+    );
+
+    config.plugins.push(
+      react({
+        exclude: [/\.stories\.(t|j)sx?$/, /node_modules/],
+        jsxImportSource: "@emotion/react",
+        babel: {
+          plugins: ["@emotion/babel-plugin"],
+        },
+      })
+    );
+
+    console.log(config.plugins);
+    return config;
+  },
 }
