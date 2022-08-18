2a0296436799663901a2cc1d088ea9d18993eca3
lottie0914-faye <lottie0914@outlook.com>
2022-08-18 18:28:06 +0900

    chore: eslintのswitchの設定を追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,7 @@ module.exports = {
     },
     "plugins": ["react", "@typescript-eslint"],
     "rules": {
-        "indent": ["error", 4],
+        "indent": ["error", 4, { "SwitchCase": 1 }],
         "linebreak-style": ["error", "unix"],
         "quotes": ["error", "double"],
         "semi": ["error", "never"],
