6d3a1f6d42e0435d224b5ba80588b5b719467f4d
lottie0914-faye <lottie0914@outlook.com>
2022-08-19 15:19:09 +0900

    fix: change routes for diary page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css"
 import { Outlet } from "react-router-dom"
+import { Layout } from "./components/layout/Layout"
 
 function App() {
     return (
         <div className="App">
-            <Outlet />
+            <Layout>
+                <Outlet />
+            </Layout>
         </div>
     )
 }
