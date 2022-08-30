56052981e43d188211038106eb782a363d3ec926
lottie0914-faye <lottie0914@outlook.com>
2022-08-30 16:23:37 +0900

    feat: 画面を更新

--- src/pages/Diaries.tsx
+++ src/pages/Diaries.tsx
@@ -3,42 +3,57 @@ import { Diary } from "../@types/diary"
 import diariesData from "../utils/diaries.json"
 import { DiaryCard } from "../components/DiaryCard/DiaryCard"
 import { css } from "@emotion/react"
+import { Layout } from "../components/layout/Layout"
+import { DiaryFilter } from "../components/DiaryFilter/DiaryFilter"
 
 export const Diaries = () => {
     return (
-        <div>
-            <div>Diaries</div>
-            <div css={styles.diariesContainer}>
-                {diariesData.map((diary: Diary): React.ReactNode => {
-                    return <Link
-                        to={`/diary/${diary.id}`}
-                        key={diary.id}
-                    >
-                        <DiaryCard
-                            id={diary.id}
-                            title={diary.title}
-                            content={diary.content}
-                            mood={diary.mood}
-                            location={diary.location}
-                            weather={diary.weather}
-                            breakfast={diary.meals?.breakfast}
-                            lunch={diary.meals?.lunch}
-                            dinner={diary.meals?.dinner} />
+        <Layout>
+            <div css={styles.container}>
+                <div css={styles.diariesContainer}>
+                    {diariesData.map((diary: Diary): React.ReactNode => {
+                        return <Link
+                            to={`/diary/${diary.id}`}
+                            key={diary.id}
+                        >
+                            <DiaryCard
+                                id={diary.id}
+                                title={diary.title}
+                                content={diary.content}
+                                mood={diary.mood}
+                                location={diary.location}
+                                weather={diary.weather}
+                                breakfast={diary.meals?.breakfast}
+                                lunch={diary.meals?.lunch}
+                                dinner={diary.meals?.dinner} />
 
-                    </Link>
-                })}
+                        </Link>
+                    })}
+                </div>
+                <div css={styles.controlContainer}>
+                    <DiaryFilter />
+                </div>
             </div>
 
             {/* <Outlet /> */}
 
-        </div>
+        </Layout>
     )
 }
 
 const styles = {
+    container: css({
+        display: "grid",
+        gridTemplateColumns: "1fr 330px",
+        gap: "30px"
+
+    }),
     diariesContainer: css({
         display: "flex",
         flexDirection: "column",
         gap: "30px"
+    }),
+    controlContainer: css({
+
     })
 }
